213eebbce24493a12ac602a4c254bf480672ff1e
José Eduardo Brandão <zefutbrandao@gmail.com>
2020-10-18 16:56:17 -0300

    init + vlibras

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header>
+        </Header>
+
+
+
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
