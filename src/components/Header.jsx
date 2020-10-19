a232761b7d29bbf12b9b741f52d798550a8c7e44
Jose Farias <jsoevictorjop@gmail.com>
2020-10-19 01:03:17 -0300

    Links added to icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,75 @@
-import React, { Component } from 'react';
-import { AppBar, IconButton, Toolbar } from '@material-ui/core';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import YouTubeIcon from '@material-ui/icons/YouTube';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import logo from '../imgs/logo.png'
+import React, { Component } from "react";
+import { AppBar, IconButton, Toolbar } from "@material-ui/core";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import logo from "../imgs/logo.png";
 
 const appbarStyle = {
-    background: 'none'
+  background: "none",
 };
 
 const appbarWrapper = {
-    width: '80%',
-    margin: '0 auto'
+  width: "80%",
+  margin: "0 auto",
 };
 
 const iconStyle = {
-    fontSize: '2rem',
-    color: 'white',
+  fontSize: "2rem",
+  color: "white",
 };
 
-
 const logoStyle = {
-    width: '200px',
-    padding: '12px'
-}
+  width: "200px",
+  padding: "12px",
+};
 export class Header extends Component {
-
-    render() {
-        return (
-            <div>
-                <AppBar style={appbarStyle} elevation={0}>
-                    <div style={{ display: 'flex' }}>
-                        <div style={{ marginLeft: '0', marginRight: 'auto' }}>
-                            <img style={logoStyle} src={logo}></img>
-                        </div>
-                        <div style={{ marginLeft: 'auto', marginRight: '0' }}>
-                            <Toolbar style={appbarWrapper}>
-                                <IconButton>
-                                    <InstagramIcon style={iconStyle} />
-                                </IconButton>
-                                <IconButton>
-                                    <FacebookIcon style={iconStyle} />
-                                </IconButton>
-                                <IconButton>
-                                    <YouTubeIcon style={iconStyle} />
-                                </IconButton>
-                            </Toolbar>
-                        </div>
-                    </div>
-                </AppBar>
-            </div >
-        )
-    }
+  render() {
+    return (
+      <div>
+        <AppBar style={appbarStyle} elevation={0}>
+          <div style={{ display: "flex" }}>
+            <div style={{ marginLeft: "0", marginRight: "auto" }}>
+              <img style={logoStyle} src={logo}></img>
+            </div>
+            <div style={{ marginLeft: "auto", marginRight: "0" }}>
+              <Toolbar style={appbarWrapper}>
+                <a
+                  href={"https://instagram.com/the.scumbags"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <IconButton>
+                    <InstagramIcon style={iconStyle}></InstagramIcon>
+                  </IconButton>
+                </a>
+                <a
+                  href={"https://facebook.com/thescumbags"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <IconButton>
+                    <FacebookIcon style={iconStyle} />
+                  </IconButton>
+                </a>
+                <a
+                  href={
+                    "https://www.youtube.com/channel/UC-bImr91y27m2lYhWwKEdRw"
+                  }
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <IconButton>
+                    <YouTubeIcon style={iconStyle} />
+                  </IconButton>
+                </a>
+              </Toolbar>
+            </div>
+          </div>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
-export default Header
+export default Header;
