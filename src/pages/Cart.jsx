39387bb9f1e64e2e55872ba95030f90ccdb305d3
Jose Farias <jsoevictorjop@gmail.com>
2020-12-30 00:01:35 -0300

    Works on cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,93 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import HeaderStore from "../components/HeaderStore";
+import data from "../data";
+import boxGuy from "../imgs/boxguy.png";
+import cyanButton from "../imgs/cyanButton.png";
+import greyButton from "../imgs/greyButton.png";
+import "../styles/pages/cart.css";
 
 function Cart() {
   return (
-    <div>
-      <h1>CART</h1>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div>
+        <HeaderStore />
+      </div>
+      <div>
+        <div style={{ display: "flex", flexDirection: "collumn" }}>
+          <div className="boxGuyContainer" style={{ flex: 1 }}>
+            <img
+              style={{ height: "300px" }}
+              className="boxGuy"
+              alt=""
+              src={boxGuy}
+            ></img>
+          </div>
+          <div style={{ flex: 5, paddingRight: "150px" }}>
+            <div>
+              <table style={{ width: "100%" }}>
+                {data.products.map((i) => (
+                  <tr
+                    style={{
+                      display: "flex",
+                    }}
+                  >
+                    <td style={{ flex: 1, padding: 0 }}>
+                      <img
+                        style={{ width: "100px" }}
+                        src={i.url}
+                        alt={i.name}
+                      ></img>
+                    </td>
+                    <td
+                      style={{
+                        flex: 5,
+                        padding: 0,
+                        position: "relative",
+                      }}
+                    >
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          margin: "20px",
+                        }}
+                      >
+                        <span className="shirt-name-style">{i.name}</span>
+                        <span className="shirt-size-style">
+                          Tamanho: {i.sizes}
+                        </span>
+                      </div>
+                    </td>
+                    <td style={{ flex: 2, padding: 0, position: "relative" }}>
+                      <span className="shirt-price-style">R$: {i.price}</span>
+                    </td>
+                    <td style={{ flex: 1, padding: 0, position: "relative" }}>
+                      <button>
+                        <span className="remove-shirt-style">REMOVER</span>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </table>
+            </div>
+            <div style={{ marginTop: "20px", display: "flex", float: "right" }}>
+              <div className="navButtonContainer">
+                <Link to={"/store"}>
+                  <img alt="" src={greyButton}></img>
+                  <span className="navButtonText">VOLTAR</span>
+                </Link>
+              </div>
+              <div className="navButtonContainer">
+                <Link to={"/checkout"}>
+                  <img alt="" src={cyanButton}></img>
+                  <span className="navButtonText">CHECKOUT</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
-import Header from "../components/Header";
 import "../styles/pages/home.css";
 import { Typography } from "@material-ui/core";
 
@@ -9,34 +8,37 @@ import capaCDgif from "../imgs/cdHomePage.gif";
 import windowsXP from "../imgs/windowsXP.png";
 import OnlineStoreButtongif from "../imgs/online-store-button.gif";
 import OnlineStoreButtonhover from "../imgs/online-store-button-hover.png";
+import logo from "../imgs/logo.png";
 
 function Home() {
   return (
-    <div id='home'>
-      <Header />
-      <div class='container-fluid' style={{ marginTop: "150px" }}>
-        <div class='row no-gutters' style={{ alignItems: "center" }}>
-          <div class='col-sm' className='section1'>
-
-
+    <div id="home">
+      <div style={{ display: "flex", background: "black" }}>
+        <div style={{ margin: "0 auto" }}>
+          <img className="logo-style" src={logo} alt="" />
+        </div>
+      </div>
+      <div class="container-fluid" style={{ marginTop: "150px" }}>
+        <div class="row no-gutters" style={{ alignItems: "center" }}>
+          <div class="col-sm" className="section1">
             <Typography></Typography>
-            <img src={windowsXP} alt='' />
+            <img src={windowsXP} alt="" />
           </div>
-          <div class='col-sm' className='section2'>
-            <Typography class='acesse'>Acesse a nossa</Typography>
+          <div class="col-sm" className="section2">
+            <Typography class="acesse">Acesse a nossa</Typography>
             <Link to={{ pathname: "/store" }}>
               <img
-                class='active'
+                class="active"
                 src={OnlineStoreButtongif}
-                alt=''
+                alt=""
                 style={{ height: "84px", width: "500px" }}
               />
             </Link>
             <div></div>
             <img
-              class='OnlineStoreButtonhover'
+              class="OnlineStoreButtonhover"
               src={OnlineStoreButtonhover}
-              alt=''
+              alt=""
             />
           </div>
         </div>
