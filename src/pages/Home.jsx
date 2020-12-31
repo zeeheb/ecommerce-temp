8024409dbc2d4a379f0632d12d04fc9ff6620702
Jose Farias <jsoevictorjop@gmail.com>
2020-12-31 14:12:31 -0300

    home.jsx fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Footer from "../components/Footer";
 import "../styles/pages/home.css";
 import { Typography } from "@material-ui/core";
 
 import capaCDgif from "../imgs/cdHomePage.gif";
 import windowsXP from "../imgs/windowsXP.png";
-import OnlineStoreButtongif from "../imgs/online-store-button.gif";
-import OnlineStoreButtonhover from "../imgs/online-store-button-hover.png";
+import onlineStoreButtongif from "../imgs/online-store-button.gif";
+import onlineStoreButtonhover from "../imgs/online-store-button-hover.png";
 import logo from "../imgs/logo.png";
+import atGif from "../imgs/atGif.gif";
+import cartGif from "../imgs/carrinho/carrinho.gif";
 
 function Home() {
+  const [storeButton, setStoreButton] = useState(onlineStoreButtongif);
+
   return (
     <div id="home">
       <div style={{ display: "flex", background: "black" }}>
@@ -18,28 +21,60 @@ function Home() {
           <img className="logo-style" src={logo} alt="" />
         </div>
       </div>
-      <div class="container-fluid" style={{ marginTop: "150px" }}>
-        <div class="row no-gutters" style={{ alignItems: "center" }}>
-          <div class="col-sm" className="section1">
-            <Typography></Typography>
+      <div style={{ marginTop: "150px" }}>
+        <div style={{ alignItems: "center", display: "flex" }}>
+          <div className="section1">
             <img src={windowsXP} alt="" />
           </div>
-          <div class="col-sm" className="section2">
+          <div className="section2">
             <Typography class="acesse">Acesse a nossa</Typography>
             <Link to={{ pathname: "/store" }}>
-              <img
-                class="active"
-                src={OnlineStoreButtongif}
-                alt=""
-                style={{ height: "84px", width: "500px" }}
-              />
+              <div className="OnlineStoreButton">
+                <div>
+                  <img
+                    onMouseOver={() => setStoreButton(onlineStoreButtonhover)}
+                    onMouseOut={() => setStoreButton(onlineStoreButtongif)}
+                    src={storeButton}
+                    alt=""
+                  ></img>
+                </div>
+              </div>
             </Link>
-            <div></div>
-            <img
-              class="OnlineStoreButtonhover"
-              src={OnlineStoreButtonhover}
-              alt=""
-            />
+            <div
+              style={{
+                display: "flex",
+                margin: "20px",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  margin: "0 auto",
+                }}
+              >
+                <div>
+                  <img
+                    src={atGif}
+                    alt=""
+                    style={{
+                      height: "83.2px",
+                      width: "79.2px",
+                      margin: "20px",
+                    }}
+                  />
+                </div>
+                <div>
+                  <img
+                    src={cartGif}
+                    alt=""
+                    style={{
+                      height: "134.3997344970703px",
+                      width: "111.99977111816406px",
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
